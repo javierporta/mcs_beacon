60ae16cb5134ed8e9067f4e5f7a016ff9402499d
Javier <2190271@my.ipleiria.pt>
2020-04-14 19:42:28 +0100

    Update readme

---
+++ bleno-example/ibeacon.js
@@ -0,0 +1,32 @@
+console.log("enter");
+
+var bleno = require("bleno");
+
+bleno.on("stateChange", onStateChange);
+
+var uuid = "e2c56db5dffb48d2b060d0f5a71096e0";
+var major = 0; // 0x0000 - 0xffff
+var minor = 0; // 0x0000 - 0xffff
+var measuredPower = -59; // -128 - 127
+
+function onStateChange(state) {
+  console.log("State is " + state);
+
+  if (state === "poweredOn") {
+    bleno.startAdvertisingIBeacon(
+      uuid,
+      major,
+      minor,
+      measuredPower,
+      onStartAdverstising
+    );
+  }
+}
+
+function onStartAdverstising(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log("Started advertising");
+  }
+}
