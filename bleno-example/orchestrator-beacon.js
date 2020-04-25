76c01edf69d4dda78873578a3c678adaa72478d8
Javier <2190271@my.ipleiria.pt>
2020-04-25 11:50:21 +0100

    Swap between broadcasting and connectable mode

--- bleno-example/orchestrator-beacon.js
+++ bleno-example/orchestrator-beacon.js
@@ -1,44 +1,83 @@
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
 function runEddystoneBeacon(exec) {
-    console.log(`exec runEddystoneBeacon`);
-    exec('node eddystone-beacon.js', (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          return;
-        }
-      
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
+  console.log(`exec runEddystoneBeacon`);
+  exec("node eddystone-beacon.js", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
-  
 
 function runIBeacon(exec) {
-    console.log(`exec runIBeacon`);
-    exec('node ibeacon.js', (err, stdout, stderr) => {
-        if (err) {
-          console.error(`exec error: ${err}`);
-          return;
-        }
-      
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
+  console.log(`exec runIBeacon`);
+  exec("node ibeacon.js", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+}
+
+function runConnectable() {
+  console.log(`exec connectable mode`);
+  exec("node main.js", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
 
 function hciconfigReset(exec) {
   console.log(`exec hciconfig hci0 reset`);
-  exec('hciconfig hci0 reset', (err, stdout, stderr) => {
-      if (err) {
-        console.error(`exec error: ${err}`);
-        return;
-      }
-    
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
+  exec("hciconfig hci0 reset", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
 
-setInterval(runEddystoneBeacon, 1500, exec);
-setInterval(runIBeacon, 1500, exec);
+const keypress = async () => {
+  process.stdin.setRawMode(true);
+  return new Promise((resolve) =>
+    process.stdin.once("data", () => {
+      process.stdin.setRawMode(false);
+      resolve();
+    })
+  );
+};
+
+(async () => {
+  var refreshIntervalIdEddystone = setInterval(runEddystoneBeacon, 1500, exec);
+  var refreshIntervalIdBeacon = setInterval(runIBeacon, 1500, exec);
+  console.log("Broadcasting, press any key to continue");
+  await keypress();
+
+  clearInterval(refreshIntervalIdEddystone);
+  clearInterval(refreshIntervalIdBeacon);
+  runConnectable();
+  console.log("Connectable mode, press any key to continue");
+  await keypress();
+
+  setInterval(runEddystoneBeacon, 1500, exec);
+  setInterval(runIBeacon, 1500, exec);
+  console.log("Broadcasting, press any key to end");
+  await keypress();
+
+  console.log("bye");
+})().then(process.exit);
